9eaa3844f355b6d8e0aad021127c122dcfc7d8c9
Gegardus <stgegard@gmail.com>
2022-05-04 18:03:59 +0400

    Set up controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'users#index'
+  get 'users/index', to: 'users#index'
+  get 'users/:user_id', to: 'users#show'
+  get 'users/:user_id/posts', to: 'posts#index'
+  get 'users/:user_id/posts/:id', to: 'posts#show'
 end
+
