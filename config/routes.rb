740a4e636301b5e5c8ee926bfaf004b8a66587cd
Gegardus <stgegard@gmail.com>
2022-05-04 18:58:42 +0400

    Fix bug suggested by the reviewer

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@
 
 Rails.application.routes.draw do
   root 'users#index'
-  get 'users/index', to: 'users#index'
+  get 'users', to: 'users#index'
   get 'users/:user_id', to: 'users#show'
   get 'users/:user_id/posts', to: 'posts#index'
   get 'users/:user_id/posts/:id', to: 'posts#show'
 end
-
