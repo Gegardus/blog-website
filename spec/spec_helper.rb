c9a9dc748676f540b3c64c53781b6c149cf84d83
Gegardus <stgegard@gmail.com>
2022-05-05 15:22:17 +0400

    Fix lynters

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -92,5 +92,5 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
-# rubocop: enable Style/BlockComments
+  # rubocop: enable Style/BlockComments
 end
