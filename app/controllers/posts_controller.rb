fd92ff819120863c25b6d20e00a8e269732ad011
Gegardus <stgegard@gmail.com>
2022-05-12 23:16:02 +0400

    Add forms

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,25 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.author = current_user
+
+    if @post.save
+      redirect_to user_path(id: @post.author_id)
+    else
+      render :new, alert: 'An error has occurred while creating the post'
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end  
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,7 @@
 class Comment < ApplicationRecord
-  belongs_to :author, class_name: 'User'
+  after_save :comments_count
+  
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post
 
   def comments_count
