84a798314345009c32196f66b91b5da2bb016408
Gegardus <stgegard@gmail.com>
2022-05-14 19:35:57 +0400

    Fix bug

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -7,13 +7,12 @@ class LikesController < ApplicationController
     @post = Post.find(params[:post_id])
     @new_like = current_user.likes.new(author_id: current_user.id, post_id: @post.id)
 
-    respond_to do |format|
+    respond_to do
       if @new_like.save
         redirect_to "/users/#{@post.author_id}/posts/#{@post.id}"
         flash[:notice] = 'Your like was added'
       else
-        redirect_to "/users/#{@post.author_id}/posts/#{@post.id}",
-        alert: 'An error has occurred while adding like'
+        redirect_to "/users/#{@post.author_id}/posts/#{@post.id}", alert: 'An error occurred adding like'
       end
     end
   end
