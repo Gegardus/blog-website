86915d3f1435d0a396417d572f0a06b3edfc3cd0
Gegardus <stgegard@gmail.com>
2022-05-24 00:49:02 +0400

    Add auth for admin and user

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -17,6 +17,14 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @comment = Comment.find(params[:id])
+    @post = @comment.post
+    @post.decrement!(:comments_counter)
+    @comment.destroy!
+    redirect_to user_post_path(id: @post.id), notice: 'Comment was successfully deleted!'
+  end
+
   private
 
   def comment_params
