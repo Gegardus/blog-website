fc89b737337530e0a7f33bd10a12b54cf8296842
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2022-05-24 18:52:07 +0500

    :art: Updated ability file

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,8 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new # guest user (not logged in)    
+    user ||= User.new # guest user (not logged in)
+    can :read, :all    
     if user.admin?
       can :manage, :all
     else
