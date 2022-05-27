e33861f5051b3177697e487e57e861acd130ab58
Gegardus <stgegard@gmail.com>
2022-05-27 22:43:06 +0400

    Generate documentation for an API

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,17 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   devise_for :users
   root 'users#index'
-
-  # Create an API endpoint to list all posts for a user.
-  # Create an API endpoint to list all comments for a user's post.
-  # Create an API endpoint to add a comment to a post. The owner of the comment is the user that makes it.
-  # Add authentication to your API endpoints.
-  # Your API endpoints should receive JSON and respond JSON as well.
 
   namespace :api , defaults: { format: :json } do
     namespace :v1 do
       post 'users/sign_up' => 'users#register'
       post 'users/sign_in' => 'users#login'
       get 'posts' => 'posts#index'
+      post 'posts/create' => 'posts#create'
       get 'comments' => 'comments#index'
       post 'comments/create' => 'comments#create'
     end
