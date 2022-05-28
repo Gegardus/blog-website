f2a8e72eceebaddc40d5192876f9f12b038fcc29
Gegardus <stgegard@gmail.com>
2022-05-28 18:37:15 +0400

    Update styling

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
   devise_for :users
-  root 'users#index'
+  root 'users#index'  
 
   namespace :api , defaults: { format: :json } do
     namespace :v1 do
@@ -21,11 +21,11 @@ Rails.application.routes.draw do
     resources :posts, only: %i[index show new create destroy] do
       resources :comments, only: %i[new create destroy]
       resources :likes, only: %i[new create]
-    end
+    end 
   end
 
   # get 'users', to: 'users#index'
   # get 'users/:user_id', to: 'users#show'
   # get 'users/:user_id/posts', to: 'posts#index'
-  # get 'users/:user_id/posts/:id', to: 'posts#show'
+  # get 'users/:user_id/posts/:id', to: 'posts#show'  
 end
